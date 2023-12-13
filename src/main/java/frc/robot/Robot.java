306c619945a6ceb71efefbd65981d794d12e7d10
SomeGuyWithGithub <129338880+SomeGuyWithGithub@users.noreply.github.com>
2023-12-13 15:35:35 -0600

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,57 +17,59 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
  * directory.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
-  private final XboxController m_controller = new XboxController(0);
-  private final Timer m_timer = new Timer();
+    private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
+    private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
+    private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
+    private final XboxController m_controller = new XboxController(0);
+    private final Timer m_timer = new Timer();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightDrive.setInverted(true);
-  }
-
-  /** This function is run once each time the robot enters autonomous mode. */
-  @Override
-  public void autonomousInit() {
-    m_timer.restart();
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
-      // Drive forwards half speed, make sure to turn input squaring off
-      m_robotDrive.arcadeDrive(0.5, 0.0, false);
-    } else {
-      m_robotDrive.stopMotor(); // stop robot
+    /**
+    * This function is run when the robot is first started up and should be used for any
+    * initialization code.
+    */
+    @Override
+    public void robotInit() {
+        // We need to invert one side of the drivetrain so that positive voltages
+        // result in both sides moving forward. Depending on how your robot's
+        // gearbox is constructed, you might have to invert the left side instead.
+        m_rightDrive.setInverted(true);
     }
-  }
 
-  /** This function is called once each time the robot enters teleoperated mode. */
-  @Override
-  public void teleopInit() {}
+    /** This function is run once each time the robot enters autonomous mode. */
+    @Override
+    public void autonomousInit() {
+        m_timer.restart();
+    }
 
-  /** This function is called periodically during teleoperated mode. */
-  @Override
-  public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
-  }
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {
+        // Drive for 2 seconds
+        if (m_timer.get() < 2.0) {
+            // Drive forwards half speed, make sure to turn input squaring off
+            m_robotDrive.arcadeDrive(0.5, 0.0, false);
+        } else {
+            m_robotDrive.stopMotor(); // stop robot
+        }
+    }
 
-  /** This function is called once each time the robot enters test mode. */
-  @Override
-  public void testInit() {}
+    /** This function is called once each time the robot enters teleoperated mode. */
+    @Override
+    public void teleopInit() {}
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This function is called periodically during teleoperated mode. */
+    @Override
+    public void teleopPeriodic() {
+        m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getRightX());
+    }
+
+    /** This function is called once each time the robot enters test mode. */
+    @Override
+    public void testInit() {}
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {}
 }
+
+// big test
